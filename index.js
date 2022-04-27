ff5534337c64cceebd84962b38ac8d3764216880
Habibur Rahman <104246685+HabibTheAlien@users.noreply.github.com>
2022-04-27 13:17:51 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var resultField=$("#result");
+function insertNumber(number){
+    var existingNumbers= resultField.val();
+    resultField.val(existingNumbers+ number)
+}
+
+function clearResult(){
+    resultField.val("");
+
+}
+
+function calculate(){
+var result=eval(resultField.val())   
+resultField.val(result);
+
+}
+function deletNumbers(){
+var presentValue= resultField.val();
+if(presentValue!='')
+resultField.val(resultField.val().slice(0,-1));
+}
